1ff679b6180f9a8dfa09deac4b78228338e7fa96
Xiu_Er <13813641925@163.com>
2020-09-13 11:18:38 +0800

    更新测试类的注释
    修改log4j的配置，不输出文件

--- src/main/java/processor/GithubRepoPageProcessor.java
+++ src/main/java/processor/GithubRepoPageProcessor.java
@@ -10,11 +10,20 @@ public class GithubRepoPageProcessor implements PageProcessor {
     public static final String URL_LIST = "http://blog\\.sina\\.com\\.cn/s/articlelist_1487828712_0_\\d+\\.html";
 
     public static final String URL_POST = "http://blog\\.sina\\.com\\.cn/s/blog_\\w+\\.html";
-
+    // 地点；位置；场所
     private Site site = Site
+            //获取Site实例
             .me()
+            /**
+             * 设置域名
+             */
             .setDomain("blog.sina.com.cn")
+            /** 设置sleep时间*/
             .setSleepTime(3000)
+            /**
+             * User Agent中文名为用户代理，简称 UA，它是一个特殊字符串头，使得服务器能够识别客户使用的操作系统及版本、CPU 类型、浏览器及版本、浏览器渲染引擎、浏览器语言、浏览器插件等。
+             *一些网站常常通过判断 UA 来给不同的操作系统、不同的浏览器发送不同的页面，因此可能造成某些页面无法在某个浏览器中正常显示，但通过伪装 UA 可以绕过检测。
+             */
             .setUserAgent(
                     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_2) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.65 Safari/537.31");
 
